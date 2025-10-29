cd75f4d889b8b5c17bf6b04ebf0cea106902af5d
Rafael <rafaelmazui53@gmail.com>
2025-10-29 10:30:44 -0300

    adicionando alguns exercicios que fiz

---
+++ desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,43 @@
+//Exercício 1
+/*let respostaDoUsario = prompt("qual dia da semana?");
+
+if (respostaDoUsario == "Sabado" || respostaDoUsario == "Domingo"){
+    alert("Bom fim de semana!");;
+} else {
+    alert("Boa semana");
+}
+
+//Exercício 2
+let numeroDoUsario = prompt ("digite um número: ");
+
+if (numeroDoUsario < 0){ 
+    alert("este numero e negativo");
+} else {
+    alert ("este numero e positivo");11
+}
+
+//Exercício 3
+let pontucao = prompt ("qual a sua pontuação?");
+
+if (pontucao >= 100) {
+    alert ("Parabéns, você venceu!");
+} else {
+    alert("Tente novamente para ganhar.");
+
+
+//Exercício 4
+
+let saldoDoUsario = 1000
+alert (`Seu saldo e de 500 ${saldoDoUsario}`)
+
+}}
+
+//Exercício 5
+
+let nomeDoUsario = prompt ("qual seu nome?");
+
+if (nomeDoUsario) {
+    alert (`boas vindas ${nomeDoUsario}`);
+} else {
+   alert ("Tente outra vez");
+}*/
