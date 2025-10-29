7cee8ed10e99a42a2ee48e904b861b1325b8b419
Rafael <rafaelmazui53@gmail.com>
2025-10-29 08:06:17 -0300

    First commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,31 @@
+alert("Boas vindas ao nosso site!");
+
+
+let nome = "lua";
+
+
+let idade = 25;
+
+
+let numeroDeVendas = 50;
+
+
+let saldoDisponivel = 1000;
+
+
+let mensagemDeErro = alert("Erro! Preencha todos os campos");
+
+
+let nome2 = prompt("Qual seu nome?");
+
+
+let idade2 = prompt("Qual sua idade?");
+
+
+if (idade2 >= 18) {
+    alert("Pode tirar a habilitação");
+
+
+} else {
+    alert ("Não pode tirar a habilitação ainda!");
+}
